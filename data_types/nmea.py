29bbef219555f3c2c0c61266d168e1a6bc7512d3
Stanislav Katsyuk <stankatsyuk@Stanislavs-MacBook-Pro.local>
2023-10-08 23:31:32 -0700

    Add nmea data types

---
+++ data_types/nmea.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class NMEASentence(Enum):
+    GPGGA = "$GPGGA"
+    GPRMC = "$GPRMC"
+    GPGSA = "$GPGSA"
+    GNGSA = "$GNGSA"
+
+class NMEAField(Enum):
+    UTC_TIME = 1 
+    LATITUDE = 2
+    LONGITUDE = 3
+    SATELLITES_TRACKED = 7
+    FIX_STATUS = 6
