f7696aa59558c7d97ee6130669822e07c21e130a
Stanislav Katsyuk <stankatsyuk@Stanislavs-MacBook-Pro.local>
2023-10-09 01:56:39 -0700

    Add plotter utils

---
+++ presentation/data_plotter.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+from typing import Optional
+
+class DataPlotter:
+    def plot_data(self, timestamps: list[float], satellite_counts: list[int], ttff: Optional[float]):
+        """
+        Plot the data showing the number of satellites tracked as a function of time.
+
+        Args:
+            timestamps (List[float]): List of timestamps (in seconds).
+            satellite_counts (List[int]): List of the number of satellites tracked at each timestamp.
+
+        Returns:
+            None
+        """
+        # Plot the data and assign labels
+        plt.plot(timestamps, satellite_counts, '-o', label='Satellites Tracked')
+        if ttff is not None:
+            plt.axvline(ttff, color='red', linestyle='--', label='Time to First Fix (TTFF)')
+
+        plt.xlabel('Time (seconds)')
+        plt.ylabel('Number of Satellites')
+        plt.title('Satellites Tracked vs Time')
+        plt.grid(True)
+        
+        # Create a legend
+        plt.legend()
+        
+        # Show the plot
+        plt.show()
+
+    def plot_ttff(self, ttff: float):
+        plt.axvline(x=ttff, color='r', linestyle='--', label=f'TTFF: {ttff} s')
+        plt.xlabel('Time (seconds)')
+        plt.title('Time to First Fix (TTFF)')
+        plt.grid(True)
+        plt.legend()
+        plt.show()
